f9c23bf9a748300cf669c5cc0d9fda9f1fea8ee0
aasergienko <31881641+aasergienko@users.noreply.github.com>
2017-10-01 19:53:25 +0300

    Add files via upload

---
+++ hw2/homework2.py
@@ -0,0 +1,16 @@
+
+print("Введите слово")
+a=input()
+if len(a)!= 0:
+
+    i = len(a)-1
+    while i >= 0:
+        if(a[i]!='з' and a[i]!='я'):
+            print(a[i])
+        i = i - 1
+    
+
+    
+        
+
+    
