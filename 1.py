a8e6c3cb34309bcc65e6659ee5a2da8524eafa43
aasergienko <31881641+aasergienko@users.noreply.github.com>
2017-11-14 11:53:33 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,5 +1,13 @@
-x = int(input('Введите число'))
-if x > 5:
-    print('Больше пяти'+ str (x))
-else:
-    print('Не больше пяти' + str(x))
+with open("text.txt") as f:
+    text=f.read()
+    print(text.count(". "))
+    a=int(0)
+    b=int(0)
+    for letter in text:
+        if letter == 'а' or letter == 'о' or letter =='е' or letter == 'у' or letter == 'ю' or letter == 'ы' or letter == 'я' or letter == 'ё'or letter == 'э':
+            a=a+1
+        else:
+            b=b+1
+
+    print(a, b)
+print(text.count(""))
